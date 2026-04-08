fede1fdeb4d26c228b10b9c1945708183a26cfeb
Aykay-o <126713029+Aykay-o@users.noreply.github.com>
2026-04-08 23:21:52 +0100

    got functionality for both creating and deletion of announcements, added seperate card component for announcements

--- src/components/InternalAnnouncement.jsx
+++ src/components/InternalAnnouncement.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "../css/InternalAnnouncement.css";
 import { AnnouncementStatus } from "../services/mockPortalData";
+import PublishedAnnouncement from "./PublishedAnnouncement";
 
 //PLEASE READ THIS BEFORE IMPLEMENTING: instead of actually deleting the existing announcement, you can create a function that will
 // .. show the status "PUBLISHED" announcements only to employees to eliminate announcement with status DELETED
 const InternalAnnouncement = ({ repository, setRepository, user }) => {
-
+  
+  // states
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  
   // handle submit of announcement
   const handlePublish = (e) => {
     e.preventDefault();
@@ -33,41 +38,64 @@ const InternalAnnouncement = ({ repository, setRepository, user }) => {
     setContent("");
   }
 
-  // states
-  const [title, setTitle] = useState();
-  const [content, setContent] = useState();
+  const handleDelete = (aID) => {
+    const newRepo = repository.map((announcement) => {
+      if (announcement.announcementID === aID) {
+        const newAnnouncement = {...announcement, announcementStatus : AnnouncementStatus.DELETED};
+        return newAnnouncement; 
+      }
+      return announcement;
+    })
 
-  // 
+    setRepository(newRepo);
+  }
+
 
   if (!user) {
     return <p>Loading user data</p>;
   }
   return (
-    <div className="AnnouncementContainer">
+    <div className="announcement-main">
       <h2>Internal Announcement</h2>
       <p>
         Logged in as <b>{user.name}</b>
       </p>
 
-      <form onSubmit={handlePublish} action="">
-        <input 
-          type="text"
-          value={title}
-          onChange={(e)=>setTitle(e.target.value)} 
-        />
-        <textarea 
-          name="" 
-          id=""
-          value={content}
-          onChange={(e)=> setContent(e.target.value)}
-        >
+      <div className="announcement-container">
+        <div className="ann-left-col">
+          {/* Create Announcement */}
+          <form onSubmit={handlePublish} action="">
+            <input 
+              type="text"
+              value={title}
+              onChange={(e)=>setTitle(e.target.value)} 
+            />
+            <textarea 
+              name="" 
+              id=""
+              value={content}
+              onChange={(e)=> setContent(e.target.value)}
+            >
 
-        </textarea>
-        <button
-          type="submit"
-        >Submit
-        </button>
-      </form>
+            </textarea>
+            <button
+              type="submit"
+            >Submit
+            </button>
+          </form>
+        </div>
+
+        <div className="ann-right-col">
+          {/* published announcements */}
+          <div className="published-cards">
+            {repository
+            .filter((a) => a.announcementStatus === AnnouncementStatus.PUBLISHED)
+            .map((announcement, index) => (
+              <PublishedAnnouncement key={index} announcement={announcement} role={user.role} handleDelete={handleDelete} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
