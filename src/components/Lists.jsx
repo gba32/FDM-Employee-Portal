399253bc7ceee67584798b9c510b6ba8699efeef
gba32 <gabrielbasusiasaam@gmail.com>
2026-04-09 21:23:39 +0100

    Added documentation to Lists and ModifyAccess files.

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -3,8 +3,7 @@
  * Provides a distance metric for a prefix and string. Does not satisfy d(p, s) == d(s, p) unless s === p 
  * @param {string} prefix the prefix that string must start with  
  * @param {string} string the string to find the distance for
- * @returns {number} Infinity if string doesn't have the given prefix,
- * otherwise the number of characters needed to be added to the prefix to get the string
+ * @returns {number} Infinity if string doesn't have the given prefix, otherwise the number of characters needed to be added to the prefix to get the string
  */
 function prefixDistance(prefix, string) {
   if (string.length < prefix.length) {
@@ -16,18 +15,20 @@ function prefixDistance(prefix, string) {
 
 
 /**
- * @typedef {object} SearchableListProps
- * @property {string} props.className
- * @property {string} props.searchValue
- * @property {*} props.items
- * @property {Function} props.templateFunction
- * @property {Function} props.nameFunction
- * @property {Function} props.keyFunction
- * @property {Function} props.distanceFunction
+ * A wrapper function providing search filtering for a list of items.
  * 
- * @param {SearchableListProps} props
+ * @typedef {object} SearchableListProps
+ * @property {string} className the css class to apply to the list
+ * @property {string} searchValue the string to filter the list based on
+ * @property {Array<*>} items The list of items to display
+ * @property {Function} templateFunction A function providing a JSX element for each item
+ * @property {Function} nameFunction A function providing a string name for each item
+ * @property {Function} keyFunction A function providing unique keys for each item
+ * @property {Function} distanceFunction A distance metric for two strings. By default this is the  {@link prefixDistance} metric.
+ * 
+ * @param {SearchableListProps} props 
  */
-function SearchableList({ className, searchValue, items, templateFunction, nameFunction, keyFunction = () => undefined, distanceFunction = prefixDistance, threshold = Infinity, limit = items.length }) {
+export function SearchableList({ className, searchValue, items, templateFunction, nameFunction, keyFunction = () => undefined, distanceFunction = prefixDistance, threshold = Infinity, limit = items.length }) {
   // Case insensitive search
   let distances = items.map((item) => distanceFunction(searchValue.toLowerCase(), nameFunction(item).toLowerCase()));
   let components = items
@@ -40,18 +41,18 @@ function SearchableList({ className, searchValue, items, templateFunction, nameF
 }
 
 /**
+ * A helper function for creating uniform jsx lists from an array of items
  * @typedef {object} ListProps
- * @property {string} props.className
- * @property {string} props.searchValue
- * @property {*} props.items
- * @property {Function} props.templateFunction
- * @property {Function} props.keyFunction
- * @property {number} limit
+ * @property {string} className the css class to apply to the list
+ * @property {Array<*>} items The list of items to display
+ * @property {Function} templateFunction A function providing a JSX element for each item
+ * @property {Function} keyFunction A function providing unique keys for each item
+ * @property {number} limit The maximum number of items to display
  * 
- * @param {ListProps} param0 
+ * @param {ListProps} props 
  * @returns 
  */
-function List({ className, items, templateFunction, limit, keyFunction = () => undefined }) {
+export function List({ className, items, templateFunction, limit, keyFunction = () => undefined }) {
   return <ul className={className}>
     {items.map((item) => <li key={keyFunction(item)}>{templateFunction(item)}</li>).slice(0, limit)}
   </ul>
