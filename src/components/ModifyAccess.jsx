bb7157f711fd1a4b1df6c7bc5fe6f9d81ee99d7b
gba32 <gba32@proton.me>
2026-04-09 10:44:22 +0100

    Started migration to MVVM.

--- src/components/ModifyAccess.jsx
+++ src/components/ModifyAccess.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import "../css/ModifyAccess.css";
 import { PermissionsType } from "../services/mockPortalData";
 
-class Permission {
+class PermissionState {
   constructor(name, enabled, editable) {
     this.name = name;
     this.enabled = enabled;
@@ -10,23 +10,86 @@ class Permission {
   }
 }
 
-function PermissionCheckBox(permission, onPermissionChanged = () => {}) {
+class EmployeeRepository {
+  /**
+   * 
+   * @param {Array} employeeData 
+   * @param {Function} setEmployeeData 
+   */
+  constructor(employeeData, setEmployeeData) {
+    this.employeeData = employeeData;
+    this.setEmployeeData = setEmployeeData;
+  }
+
+  getEmployeeById(employeeId) {
+    let employee = this.employeeData.find((item) => item.id === employeeId);
+    return employee !== undefined ? employee : null;
+  }
+
+  setEmployeePermissions(employeeId, permissionStates) {
+    let employee = this.getEmployeeById(employeeId);
+    if(employee !== null) {
+      employee.permissions = permissionStates.filter((p) => p.enabled).map((p) => p.name);
+      console.log(this.employeeData);
+      this.setEmployeeData(this.employeeData);
+      console.log(this.employeeData);
+    }
+  }
+}
+
+
+class PermissionManager {
+  /**
+   * 
+   * @param {Array<string>} permissionList 
+   * @param {EmployeeRepository} employeeRepository 
+   */
+  constructor(permissionList, employeeRepository) {
+    this.permissionList = permissionList;
+    this.employeeRepository = employeeRepository
+  }
+
+  getPermissionStates(employeeId) {
+    let employee = this.employeeRepository.getEmployeeById(employeeId);
+
+    if(employee === undefined) {
+      return null;
+    }
+
+    let permissionMap = this.permissionList.map((p) => { return { name: p, enabled: employee.permissions.includes(p) } });
+    return permissionMap.map((p) => new PermissionState(p.name, p.enabled, false))
+  }
+
+  setPermissions(employeeId, permissionStates) {
+    this.employeeRepository.setEmployeePermissions(employeeId, permissionStates);
+  }
+}
+
+
+/**
+ * 
+ * @param {*} permission 
+ * @param {*} onPermissionChanged 
+ * @returns 
+ */
+function PermissionCheckBox(permission, onPermissionChanged = () => { }) {
   return <>
     <input type="checkbox" checked={permission.enabled} disabled={permission.editable} onChange={(event) => onPermissionChanged(permission, event.target.checked)} />
     <label>{permission.name}</label>
   </>
 }
 
-function PermissionBar({ permissions, onPermissionChanged = () => {} }) {
-  return (<List className="permissionBar" items={permissions} template={(p) => PermissionCheckBox(p, onPermissionChanged)} />);
+function PermissionBar({ permissions, onPermissionChanged = () => { } }) {
+  return (<List className="permissionBar" items={permissions} templateFunction={(p) => PermissionCheckBox(p, onPermissionChanged)} />);
 }
 
 /**
+ * @typedef 
  * 
  * @param {*} param0 
  * @returns 
  */
-function SearchBar({ className, hint, onValueChanged = () => {} }) {
+function SearchBar({ className, hint, onValueChanged = () => { } }) {
   const searchRef = useRef(null);
   useEffect(() => {
     let observer = new MutationObserver((mutations) => {
@@ -44,6 +107,13 @@ function SearchBar({ className, hint, onValueChanged = () => {} }) {
   </form>);
 }
 
+/**
+ * 
+ * @param {*} props
+ * @param {string} props.className The CSS class to style the pfp with
+ * @param {*} employee The employee to display the PFP for.
+ * @returns 
+ */
 function EmployeePFP({ className, employee }) {
   return employee.logo === undefined ? <svg className={className} height={48} width={48} >
     <circle r={24} cx={24} cy={24} fill="#C5FF00" />
@@ -52,12 +122,21 @@ function EmployeePFP({ className, employee }) {
 }
 
 /**
+ * @typedef {object} EmployeeCardProps
+ * @property {string} className 
+ * @property {object} employee
+ * @property {Array} permissions
  * 
- * @param {*} param0 
+ * @param {EmployeeCardProps} props
  */
-function EmployeeCard({ className, employee, permissions, onPermissionsChanged = (newPermissions) => {} }) {
-  let permissionMap = permissions.map((p) => { return { p: p, enabled: employee.permissions.includes(p) } });
-  let [permissionState, setPermissionState] = useState(permissionMap.map((v) => new Permission(v.p, v.enabled, false)));
+function EmployeeCard({ className, employee, permissionManager, onPermissionsChanged = (newPermissions) => { } }) {
+  let [permissionState, setPermissionState] = useState(permissionManager.getPermissionStates(employee.id));
+  const updatePermissionState = (permission, newValue) => {
+    // Copy the current permissions and edit only the affected one.
+    let newPermissions = permissionState.map((p) => p === permission ? new PermissionState(p.name, newValue, p.editable) : p);
+    setPermissionState(newPermissions)
+  }
+
   return <article className={className}>
     <header>
       <EmployeePFP employee={employee} />
@@ -66,16 +145,20 @@ function EmployeeCard({ className, employee, permissions, onPermissionsChanged =
         <h6>{employee.email}</h6>
       </div>
     </header>
-    <PermissionBar permissions={permissionState} onPermissionChanged={
-      (permission, newValue) => {
-        let newPermissions = permissionState.map((p) => p === permission ? new Permission(p.name, newValue, p.editable) : p);
-        setPermissionState(newPermissions)
-        onPermissionsChanged(newPermissions);
-      }
-    } />
+    <PermissionBar permissions={permissionState} onPermissionChanged={updatePermissionState} />
+    <div className="saveBar">
+      <button onClick={() => { onPermissionsChanged(permissionState) }}>Save changes</button>
+    </div>
   </article>
 }
 
+/**
+ * Provides a distance metric for a prefix and string. Does not satisfy d(p, s) == d(s, p) unless s === p 
+ * @param {string} prefix the prefix that string must start with  
+ * @param {string} string the string to find the distance for
+ * @returns {number} Infinity if string doesn't have the given prefix,
+ * otherwise the number of characters needed to be added to the prefix to get the string
+ */
 function prefixDistance(prefix, string) {
   if (string.length < prefix.length) {
     return Infinity;
@@ -85,35 +168,66 @@ function prefixDistance(prefix, string) {
 }
 
 /**
+ * @typedef {object} SearchableListProps
+ * @property {string} props.className
+ * @property {string} props.searchValue
+ * @property {*} props.items
+ * @property {Function} props.templateFunction
+ * @property {Function} props.nameFunction
+ * @property {Function} props.keyFunction
+ * @property {Function} props.distanceFunction
  * 
- * @param {*} param0 
+ * @param {SearchableListProps} props
  */
-function SearchableList({ className, searchValue, items, template, nameFunction, distanceFunc = prefixDistance, threshold = Infinity, limit = items.length }) {
-  let distances = items.map((item) => distanceFunc(searchValue.toLowerCase(), nameFunction(item).toLowerCase()));
+function SearchableList({ className, searchValue, items, templateFunction, nameFunction, keyFunction = () => undefined, distanceFunction = prefixDistance, threshold = Infinity, limit = items.length }) {
+  // Case insensitive search
+  let distances = items.map((item) => distanceFunction(searchValue.toLowerCase(), nameFunction(item).toLowerCase()));
   let components = items
     .filter((_, index) => distances[index] < threshold)
     .map((item, index) => ({ v: item, d: distances[index] }))
     .sort((a, b) => a.d - b.d)
     .map((item) => item.v)
 
-  return <List className={className} items={components} template={template} limit={limit} />;
+  return <List className={className} keyFunction={keyFunction} items={components} templateFunction={templateFunction} limit={limit} />;
 }
 
-function List({ className, items, template, limit }) {
+/**
+ * @typedef {object} ListProps
+ * @property {string} props.className
+ * @property {string} props.searchValue
+ * @property {*} props.items
+ * @property {Function} props.templateFunction
+ * @property {Function} props.keyFunction
+ * @property {number} limit
+ * 
+ * @param {ListProps} param0 
+ * @returns 
+ */
+function List({ className, items, templateFunction, limit, keyFunction = () => undefined }) {
   return <ul className={className}>
-    {items.map((item) => <li>{template(item)}</li>).slice(0, limit)}
+    {items.map((item) => <li key={keyFunction(item)}>{templateFunction(item)}</li>).slice(0, limit)}
   </ul>
 }
 
 //PLEASE READ THIS BEFORE IMPLEMENTING: toggle permissions or Role type(changing role type is the same as giving admin rights so no need to have a
 // ..checkbox and instead implement a dropdown menu)
+
+/**
+ * @typedef {object} ModifyAccessProps
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
 const ModifyAccess = ({ repository, setRepository, user }) => {
+  const permissionList = Object.values(PermissionsType);
   let [searchValue, setSearchValue] = useState("");
-  let permissions = Object.values(PermissionsType);
-  if (!user) {
-    return <p>Loading user data</p>
-  }
+  let employeeRepository = new EmployeeRepository(repository, setRepository);
+  let permissionManager = new PermissionManager(permissionList, employeeRepository);
   
+  if (!user) {
+    return <p>Loading user data</p> 
+  }
+
   const updatePermissions = (employee, permissions) => {
     console.log(repository);
     // hackish way to update repo
@@ -135,13 +249,17 @@ const ModifyAccess = ({ repository, setRepository, user }) => {
         className="cardContainer"
         searchValue={searchValue}
         items={repository}
-        template={(item) => <EmployeeCard 
-          employee={item} 
-          permissions={permissions}
-          className="employeeCard" 
-          onPermissionsChanged={(newPermissions) => {updatePermissions(item, newPermissions.filter((p) => p.enabled).map((p) => p.name)) }} />}
-        nameFunction={(item) => item.name} limit={2} />
+        templateFunction={(item) => <EmployeeCard
+          employee={item}
+          permissions={permissionList}
+          className="employeeCard"
+          onPermissionsChanged={(newPermissions) => { 
+            permissionManager.setPermissions(item.id, newPermissions);
+          }} />}
+        nameFunction={(item) => item.name} limit={2}
+        keyFunction={(item) => item.id} />
     </section>
   );
 };
+
 export default ModifyAccess;
